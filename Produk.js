efbc535990c515ddcd427a367409a68ecdc173f5
29Larusdi <latifrara24@gmail.com>
2025-05-20 00:50:16 +0700

    Delete Produk.js

--- Produk.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const produkSchema = new mongoose.Schema({
-  nama: String,
-  harga: Number,
-  gambar: String
-});
-module.exports = mongoose.model('Produk', produkSchema);
